bbf545c6ecef8e51978b2de6a898026fe892ef40
Efren Mercado Jr <efren.mercado.jr@dynameyes.com>
2019-05-02 16:25:19 +0800

    Add id to login

--- useraccounts/serializers.py
+++ useraccounts/serializers.py
@@ -7,7 +7,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username',)
+        fields = ('id', 'username',)
 
 
 class UserSerializerWithToken(serializers.ModelSerializer):
